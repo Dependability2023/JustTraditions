16ef3f149904543926f64b829fd3719eacea18d5
LeluZero <giuseppesgambato301001@gmail.com>
2022-12-31 14:38:32 +0100

    added interface AnnuncioDao, FotoDao,VistiaDao,PrenotazioneDao.

---
+++ src/main/java/it/unisa/justTraditions/storage/prenotazioniStorage/dao/PrenotazioneDao.java
@@ -0,0 +1,7 @@
+package it.unisa.justTraditions.storage.prenotazioniStorage.dao;
+
+import it.unisa.justTraditions.storage.prenotazioniStorage.entity.Prenotazione;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PrenotazioneDao extends JpaRepository<Prenotazione,Long> {
+}
