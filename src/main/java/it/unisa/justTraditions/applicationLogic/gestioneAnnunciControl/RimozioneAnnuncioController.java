d2fb6f4857a93c75116899e451463162cf9966fe
losor2002 <114603230+losor2002@users.noreply.github.com>
2023-01-18 21:07:08 +0100

    Fixed AnnuncioForm, added ProvinciaIsValidConstraint and ProvinciaIsValidValidator, continued implementation of ModificaAnnuncioController

--- src/main/java/it/unisa/justTraditions/applicationLogic/gestioneAnnunciControl/RimozioneAnnuncioController.java
+++ src/main/java/it/unisa/justTraditions/applicationLogic/gestioneAnnunciControl/RimozioneAnnuncioController.java
@@ -3,6 +3,7 @@ package it.unisa.justTraditions.applicationLogic.gestioneAnnunciControl;
 import it.unisa.justTraditions.applicationLogic.autenticazioneControl.util.SessionCliente;
 import it.unisa.justTraditions.storage.gestioneAnnunciStorage.dao.AnnuncioDao;
 import it.unisa.justTraditions.storage.gestioneAnnunciStorage.entity.Annuncio;
+import it.unisa.justTraditions.storage.gestioneProfiliStorage.dao.ArtigianoDao;
 import it.unisa.justTraditions.storage.gestioneProfiliStorage.entity.Artigiano;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,29 +13,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/rimozioneannuncio")
+@RequestMapping("/rimozioneAnnuncio")
 public class RimozioneAnnuncioController {
-  private static final String NotificaPage = "";
+
+  private static final String modificaAnnuncioSuccessView =
+      "gestioneAnnunciView/modificaAnnuncioSuccess";
+
   @Autowired
-  AnnuncioDao annuncioDao;
+  private AnnuncioDao annuncioDao;
+
   @Autowired
-  SessionCliente sessionCliente;
+  private ArtigianoDao artigianoDao;
+
+  @Autowired
+  private SessionCliente sessionCliente;
 
   @GetMapping
   public String get(@RequestParam Long id) {
-
     Optional<Annuncio> optionalAnnuncio = annuncioDao.findById(id);
     if (optionalAnnuncio.isEmpty()) {
       throw new IllegalArgumentException();
     }
+
     Annuncio annuncio = optionalAnnuncio.get();
+
     Artigiano artigiano = (Artigiano) sessionCliente.getCliente().get();
     if (!annuncio.getArtigiano().equals(artigiano)) {
       throw new IllegalArgumentException();
     }
+
     artigiano.removeAnnuncio(annuncio);
+    artigianoDao.save(artigiano);
 
-
-    return NotificaPage;
+    return modificaAnnuncioSuccessView;
   }
 }
