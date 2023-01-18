9fb332b36bb6f406bb493402ebfd42cb011b38fe
LeluZero <giuseppesgambato301001@gmail.com>
2023-01-18 22:20:32 +0100

    fix iban form

--- src/main/java/it/unisa/justTraditions/applicationLogic/autenticazioneControl/form/IsArtigianoValidator.java
+++ src/main/java/it/unisa/justTraditions/applicationLogic/autenticazioneControl/form/IsArtigianoValidator.java
@@ -13,6 +13,10 @@ public class IsArtigianoValidator
   @Override
   public boolean isValid(RegistrazioneForm registrazioneForm,
                          ConstraintValidatorContext constraintValidatorContext) {
+    if (registrazioneForm.isArtigiano() && !registrazioneForm.getIban().isBlank()) {
+      return registrazioneForm.getIban()
+          .matches("^(it|IT)[0-9]{2}[A-Za-z][0-9]{10}[0-9A-Za-z]{12}$");
+    }
     return !registrazioneForm.isArtigiano() || !registrazioneForm.getIban().isBlank();
   }
 }
