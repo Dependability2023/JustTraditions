0751dff0bf53d3d58a4ede9dd7a80c55bde60ff7
Giovanni Miele <g.miele18@studenti.unisa.it>
2023-01-19 05:54:22 +0100

    Integration backend in navbar & controller visualizzalistaannunciapprovati

--- src/main/java/it/unisa/justTraditions/applicationLogic/visualizzazioneAnnunciControl/VisualizzazioneListaAnnunciApprovatiController.java
+++ src/main/java/it/unisa/justTraditions/applicationLogic/visualizzazioneAnnunciControl/VisualizzazioneListaAnnunciApprovatiController.java
@@ -4,6 +4,7 @@ import it.unisa.justTraditions.storage.gestioneAnnunciStorage.dao.AnnuncioDao;
 import it.unisa.justTraditions.storage.gestioneAnnunciStorage.entity.Annuncio;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +17,12 @@ public class VisualizzazioneListaAnnunciApprovatiController {
   @Autowired
   private AnnuncioDao annuncioDao;
 
+
   @GetMapping
   public ModelAndView get() {
-    List<Annuncio> annunci = annuncioDao.findAll();
+    Annuncio annuncio = new Annuncio();
+    annuncio.setStato(Annuncio.Stato.APPROVATO);
+    List<Annuncio> annunci = annuncioDao.findAll(Example.of(annuncio));
 
     return new ModelAndView("visualizzazioneAnnunciView/visualizzazioneListaAnnunciApprovati")
         .addObject("annunci", annunci);
