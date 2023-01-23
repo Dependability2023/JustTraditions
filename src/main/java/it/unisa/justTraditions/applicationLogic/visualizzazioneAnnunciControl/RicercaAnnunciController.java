40f904ed143b2a51f721641cceace7bd15b2afcf
Giovanni Miele <g.miele18@studenti.unisa.it>
2023-01-23 17:00:08 +0100

    controll approved or rifiuted

--- src/main/java/it/unisa/justTraditions/applicationLogic/visualizzazioneAnnunciControl/RicercaAnnunciController.java
+++ src/main/java/it/unisa/justTraditions/applicationLogic/visualizzazioneAnnunciControl/RicercaAnnunciController.java
@@ -6,12 +6,10 @@ import it.unisa.justTraditions.storage.gestioneAnnunciStorage.entity.Annuncio;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -19,53 +17,36 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/ricercaAnnunci")
 public class RicercaAnnunciController {
 
-  private static final String ricercaAnnunciView =
-      "visualizzazioneAnnunciView/ricercaAnnunci";
+    private static final String ricercaAnnunciView =
+            "visualizzazioneAnnunciView/ricercaAnnunci";
 
-  @Autowired
-  private AnnuncioDao annuncioDao;
+    @Autowired
+    private AnnuncioDao annuncioDao;
 
-  @Autowired
-  private Province province;
+    @Autowired
+    private Province province;
 
-  private int count;
+    @GetMapping
+    public String get(@RequestParam(defaultValue = "", required = false) String nomeAttivita,
+                      @RequestParam(defaultValue = "", required = false) String provincia,
+                      @RequestParam(defaultValue = "0", required = false) Integer pagina,
+                      Model model) {
+        if (!nomeAttivita.isBlank() && nomeAttivita.length() > 40) {
+            throw new IllegalArgumentException();
+        }
+        if (!provincia.isBlank() && !province.getProvince().contains(provincia)) {
+            throw new IllegalArgumentException();
+        }
 
-  @GetMapping
-  public String get(@RequestParam(defaultValue = "", required = false) String nomeAttivita,
-                    @RequestParam(defaultValue = "", required = false) String provincia,
-                    @RequestParam(defaultValue = "0", required = false) Integer pagina,
-                    Model model) {
-    if (!nomeAttivita.isBlank() && nomeAttivita.length() > 40) {
-      throw new IllegalArgumentException();
+        List<Annuncio> annunci =
+                annuncioDao.findByNomeAttivitaContainsIgnoreCaseAndProvinciaAttivitaContains(
+                        nomeAttivita,
+                        provincia,
+                        PageRequest.of(pagina, 20, Sort.by(Sort.Direction.ASC, "nomeAttivita"))
+                );
+
+        model.addAttribute("annunci", annunci);
+
+        return ricercaAnnunciView;
     }
-    if (!provincia.isBlank() && !province.getProvince().contains(provincia)) {
-      throw new IllegalArgumentException();
-    }
-
-
-    List<Annuncio> annunci =
-        annuncioDao.findByNomeAttivitaContainsIgnoreCaseAndProvinciaAttivitaContains(
-            nomeAttivita,
-            provincia,
-                PageRequest.of(pagina, 4, Sort.by(Sort.Direction.ASC, "nomeAttivita"))
-        );
-
-
-if(annunci.isEmpty()){
-
-    pagina--;
-     annunci =
-            annuncioDao.findByNomeAttivitaContainsIgnoreCaseAndProvinciaAttivitaContains(
-                    nomeAttivita,
-                    provincia,
-                    PageRequest.of(pagina, 4, Sort.by(Sort.Direction.ASC, "nomeAttivita"))
-            );
-}
-    model.addAttribute("annunci", annunci);
-   model.addAttribute("ricercaPagina",pagina);
-    model.addAttribute("ricercaNomeAttivita",nomeAttivita);
-    model.addAttribute("ricercaProvincia",provincia);
-
-    return ricercaAnnunciView;
-  }
 }
