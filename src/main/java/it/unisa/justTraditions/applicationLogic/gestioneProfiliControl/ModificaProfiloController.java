541f0d52748a1d4bb9af29615f6f3fec464542d8
losor2002 <114603230+losor2002@users.noreply.github.com>
2023-01-23 18:01:28 +0100

    Filtered and fixed ModificaProfiloController, VisualizzazioneListaAnnunciSottomessiController, VisualizzazioneProfiloPersonaleController, renamed visualizazioneListaAnnunci.html, fixed IsArtigianoValidator

--- src/main/java/it/unisa/justTraditions/applicationLogic/gestioneProfiliControl/ModificaProfiloController.java
+++ src/main/java/it/unisa/justTraditions/applicationLogic/gestioneProfiliControl/ModificaProfiloController.java
@@ -1,60 +1,39 @@
 package it.unisa.justTraditions.applicationLogic.gestioneProfiliControl;
 
 import it.unisa.justTraditions.applicationLogic.autenticazioneControl.form.RegistrazioneForm;
-import it.unisa.justTraditions.applicationLogic.autenticazioneControl.util.PasswordEncryptor;
 import it.unisa.justTraditions.applicationLogic.autenticazioneControl.util.SessionCliente;
-import it.unisa.justTraditions.storage.gestioneProfiliStorage.dao.ArtigianoDao;
-import it.unisa.justTraditions.storage.gestioneProfiliStorage.dao.ClienteDao;
 import it.unisa.justTraditions.storage.gestioneProfiliStorage.entity.Artigiano;
 import it.unisa.justTraditions.storage.gestioneProfiliStorage.entity.Cliente;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/modificaprofilo")
+@RequestMapping("/modificaProfilo")
 public class ModificaProfiloController {
 
-    @Autowired
-    private ClienteDao clienteDao;
-    @Autowired
-    private ArtigianoDao artigianoDao;
-    @Autowired
-    private PasswordEncryptor passwordEncryptor;
-    @Autowired
-    private SessionCliente sessionCliente;
+  private static final String modificaProfiloView = "gestioneProfiliView/modificaProfilo";
 
+  @Autowired
+  private SessionCliente sessionCliente;
 
-    @GetMapping
-    public ModelAndView get() {
-        RegistrazioneForm registrazioneForm = new RegistrazioneForm();
+  @GetMapping
+  public String get(@ModelAttribute RegistrazioneForm registrazioneForm) {
+    Cliente cliente = sessionCliente.getCliente().get();
+    registrazioneForm.setNome(cliente.getNome());
+    registrazioneForm.setCognome(cliente.getCognome());
+    registrazioneForm.setCodiceFiscale(cliente.getCodiceFiscale());
+    registrazioneForm.setEmail(cliente.getEmail());
 
-
-        if (sessionCliente.getCliente().get().getClass() == Artigiano.class) {
-            Artigiano artigiano = (Artigiano) sessionCliente.getCliente().get();
-            registrazioneForm.setNome(artigiano.getNome());
-            registrazioneForm.setCognome(artigiano.getCognome());
-            registrazioneForm.setCodiceFiscale(artigiano.getCodiceFiscale());
-            registrazioneForm.setEmail(artigiano.getEmail());
-            registrazioneForm.setArtigiano(true);
-            registrazioneForm.setIban(artigiano.getIban());
-
-        }else {
-            Cliente cliente = sessionCliente.getCliente().get();
-            registrazioneForm.setNome(cliente.getNome());
-            registrazioneForm.setCognome(cliente.getCognome());
-            registrazioneForm.setCodiceFiscale(cliente.getCodiceFiscale());
-            registrazioneForm.setEmail(cliente.getEmail());
-            registrazioneForm.setArtigiano(false);
-        }
-
-        return new ModelAndView("gestioneProfiliView/modificaprofilo").addObject("registrazioneForm", registrazioneForm);
+    if (cliente.getClass() == Artigiano.class) {
+      registrazioneForm.setArtigiano(true);
+      registrazioneForm.setIban(((Artigiano) cliente).getIban());
+    } else {
+      registrazioneForm.setArtigiano(false);
     }
 
-
+    return modificaProfiloView;
+  }
 }
