fe80b4f1ddde256501c456e2558afc1a9d8e6d0a
LeluZero <giuseppesgambato301001@gmail.com>
2023-01-24 17:46:05 +0100

    add VisualizzazionePrenotazioniAnnuncioController

--- src/main/java/it/unisa/justTraditions/applicationLogic/prenotazioniControl/VisualizzazionePrenotazioniAnnuncioController.java
+++ src/main/java/it/unisa/justTraditions/applicationLogic/prenotazioniControl/VisualizzazionePrenotazioniAnnuncioController.java
@@ -1,23 +1,53 @@
 package it.unisa.justTraditions.applicationLogic.prenotazioniControl;
 
-import java.sql.Date;
+import it.unisa.justTraditions.applicationLogic.autenticazioneControl.util.SessionCliente;
+import it.unisa.justTraditions.storage.gestioneAnnunciStorage.entity.Annuncio;
+import it.unisa.justTraditions.storage.gestioneProfiliStorage.entity.Artigiano;
+import it.unisa.justTraditions.storage.prenotazioniStorage.dao.PrenotazioneDao;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/visualizzazionePrenotazioniAnnuncio")
 public class VisualizzazionePrenotazioniAnnuncioController {
-
+  @Autowired
+  private SessionCliente sessionCliente;
+  @Autowired
+  private PrenotazioneDao prenotazioneDao;
   private static final String visualizzazionePrenotazioniAnnuncioView =
       "prenotazioniView/visualizzazionePrenotazioniAnnuncio";
 
   @GetMapping
-  public ModelAndView get(@RequestParam(required = false) String ricercaAttivita, Date date) {
-    System.out.println(ricercaAttivita + date);
-    return new ModelAndView(visualizzazionePrenotazioniAnnuncioView).addObject("Prenotazioni");
+  public String get(Model model) {
+    Artigiano artigiano = (Artigiano) sessionCliente.getCliente().get();
+    List<Annuncio> annunci = artigiano.getAnnunci();
+
+    model.addAttribute("annunci", annunci);
+    return visualizzazionePrenotazioniAnnuncioView;
+  }
+
+  @PostMapping
+  public String post(@RequestParam() Long id, @RequestParam() LocalDate dataVisita, Model model) {
+
+    Artigiano artigiano = (Artigiano) sessionCliente.getCliente().get();
+    List<Annuncio> annunci = artigiano.getAnnunci();
+    Optional<Annuncio> optionalAnnuncio =
+        annunci.stream().filter(annuncio -> (annuncio.getId() == id)).findFirst();
+    if (optionalAnnuncio.isEmpty()) {
+      throw new IllegalArgumentException();
+    }
+    model.addAttribute("prenotazioni",
+        prenotazioneDao.finByVisitaAnnuncioAndDataVisita(optionalAnnuncio.get(), dataVisita));
+    return visualizzazionePrenotazioniAnnuncioView;
+
   }
 }
 
