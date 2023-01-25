32eda10502bc08afa34d7409a2cc587ca0ef535e
MrMinato01 <spagnuolo.michele2001@gmail.com>
2023-01-25 16:39:16 +0100

    adjusted ModificaProfiloController

--- src/main/java/it/unisa/justTraditions/applicationLogic/gestioneProfiliControl/ModificaProfiloController.java
+++ src/main/java/it/unisa/justTraditions/applicationLogic/gestioneProfiliControl/ModificaProfiloController.java
@@ -2,12 +2,15 @@ package it.unisa.justTraditions.applicationLogic.gestioneProfiliControl;
 
 import it.unisa.justTraditions.applicationLogic.autenticazioneControl.form.RegistrazioneForm;
 import it.unisa.justTraditions.applicationLogic.autenticazioneControl.util.SessionCliente;
+import it.unisa.justTraditions.storage.gestioneProfiliStorage.dao.ClienteDao;
 import it.unisa.justTraditions.storage.gestioneProfiliStorage.entity.Artigiano;
 import it.unisa.justTraditions.storage.gestioneProfiliStorage.entity.Cliente;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,6 +21,9 @@ public class ModificaProfiloController {
 
   @Autowired
   private SessionCliente sessionCliente;
+
+  @Autowired
+  private ClienteDao clienteDao;
 
   @GetMapping
   public String get(@ModelAttribute RegistrazioneForm registrazioneForm) {
@@ -34,6 +40,38 @@ public class ModificaProfiloController {
       registrazioneForm.setArtigiano(false);
     }
 
+    return modificaProfiloView;
+  }
+
+  @PostMapping
+  public String post(@ModelAttribute RegistrazioneForm registrazioneForm) {
+
+    Optional<Cliente> optionalCliente = clienteDao.findByEmail(registrazioneForm.getEmail());
+    Cliente cliente = optionalCliente.get();
+
+    if (!cliente.getNome().equals(registrazioneForm.getNome())) {
+      cliente.setNome(registrazioneForm.getNome());
+    }
+
+    if (!cliente.getCognome().equals(registrazioneForm.getCognome())) {
+      cliente.setCognome(registrazioneForm.getCognome());
+    }
+
+    if (!cliente.getEmail().equals(registrazioneForm.getEmail())) {
+      cliente.setEmail(registrazioneForm.getEmail());
+    }
+
+    if (!cliente.getCodiceFiscale().equals(registrazioneForm.getCodiceFiscale())) {
+      cliente.setCodiceFiscale(registrazioneForm.getCodiceFiscale());
+    }
+
+    if (cliente.getClass() == Artigiano.class) {
+      registrazioneForm.setArtigiano(true);
+      ((Artigiano) cliente).setIban(registrazioneForm.getIban());
+    } else {
+      registrazioneForm.setArtigiano(false);
+    }
+    clienteDao.save(cliente);
     return modificaProfiloView;
   }
 }
