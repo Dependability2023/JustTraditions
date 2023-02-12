e1e76b436ac76a47584a5a1799d0d1ab35dab1c8
losor2002 <114603230+losor2002@users.noreply.github.com>
2023-02-12 17:06:57 +0100

    fixed javadoc

--- src/main/java/it/unisa/justTraditions/applicationLogic/gestioneAnnunciControl/ModificaStatoAnnuncioController.java
+++ src/main/java/it/unisa/justTraditions/applicationLogic/gestioneAnnunciControl/ModificaStatoAnnuncioController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
- * Implementa il controller per la modifica di uno stato del Annuncio.
+ * Implementa il controller per la modifica di uno stato dell'Annuncio.
  */
 @Controller
 @RequestMapping("/modificaStatoAnnuncio")
@@ -28,7 +28,7 @@ public class ModificaStatoAnnuncioController {
       "gestioneAnnunciView/modificaStatoAnnuncio";
 
   private static final String modificaAnnuncioSuccessView =
-      "gestioneAnnunciView/modificaStatoAnnuncio";
+      "gestioneAnnunciView/modificaAnnuncioSuccess";
 
   @Autowired
   private AnnuncioDao annuncioDao;
