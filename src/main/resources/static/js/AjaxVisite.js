610b10188d2c3fde546b062d262fd86d3344afdf
losor2002 <114603230+losor2002@users.noreply.github.com>
2023-02-13 19:30:58 +0100

    fixed AjaxVisite.js, OrarioIsValidConstraint, sottomissioneAnnuncio.html, visualizzazionePrenotazioniAnnuncio.html

--- src/main/resources/static/js/AjaxVisite.js
+++ src/main/resources/static/js/AjaxVisite.js
@@ -1,55 +1,45 @@
 $(document).ready(function () {
 
-    $("#dataVisita").blur(function () {
-        var data = $("#dataVisita").val();
-        var today = new Date();
-        var day = today.getDate();
-        var month = today.getMonth() + 1;
-        var year = today.getFullYear();
-        if(day> 0 && day<= 9){
-            day="0"+day;
-        }
-        if(month> 0 && month<= 9){
-            month="0"+month;
-        }
-        var dataodierna = year+'-'+month+'-'+day;
+        $("#dataVisita").blur(function () {
+            var listaVisite = $("#listavisite");
+            listaVisite.empty();
 
-        $("#listavisite").empty();
-
-
-        if(data == ""){
-            return;
-        }
-
-        if(dataodierna >= data){
-            $("#listavisite").append('<option>Data nel passato</option>');
-            return
-        }
-
-        var mydata = {dataVisita: data, idAnnuncio: $("#idAnnuncio").val()};
-        $("#listavisite").empty();
-        $.ajax({
-            url: '/ricercaVisita',
-            method: 'Post',
-            data: mydata,
-            success: function (data) {
-
-
-                if(data.length == 0){
-                    $("#listavisite").append('<option>Nessuna visita</option>');
-                }else{
-                    $("#listavisite").append('<option>Seleziona una visita</option>');
-                    data.map(visita =>{
-                        $("#listavisite").append('<option value="'+visita.id+'">'+visita.orarioInizio+'-'+visita.orarioFine+'</option>');
-
-                    })
-                }
+            if ($(this).val() === "") {
+                return;
             }
+
+            var data = Date.parse($(this).val());
+            var today = new Date().getTime();
+
+            if (isNaN(data)) {
+                listaVisite.append('<option>Formato data non valido</option>');
+                return;
+            }
+
+            if (today >= data) {
+                listaVisite.append('<option>Data nel passato</option>');
+                return;
+            }
+
+            var mydata = {dataVisita: $(this).val(), idAnnuncio: $("#idAnnuncio").val()};
+            $.ajax({
+                url: '/ricercaVisita',
+                method: 'Post',
+                data: mydata,
+                success: function (data) {
+
+
+                    if (data.length == 0) {
+                        $("#listavisite").append('<option>Nessuna visita</option>');
+                    } else {
+                        $("#listavisite").append('<option>Seleziona una visita</option>');
+                        data.map(visita => {
+                            $("#listavisite").append('<option value="' + visita.id + '">' + visita.orarioInizio + '-' + visita.orarioFine + '</option>');
+
+                        })
+                    }
+                }
+            });
         });
-    });
-}
-
-
-
-
+    }
 );
